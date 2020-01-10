14c6ae3ad2d142abf2c057e60a11d1219f3d4c25
Arthur Zachow <arthur.zachow@gmail.com>
2020-01-10 00:56:47 -0300

    Finished unit test implementation for instructions.

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -10,7 +10,7 @@ namespace Neander
 {
 	namespace Tests
 	{
-		TEST_CLASS(ComputerTests)
+		TEST_CLASS(ComputerInstructionTests)
 		{
 		public:
 			TEST_METHOD(NOP_Instruction)
@@ -654,6 +654,418 @@ namespace Neander
 
 				// Assert PC value
 				Assert::AreEqual(static_cast<int>(c_finalRegs.m_programCounter), 2, L"PC has wrong value.");
+
+				// Assert NEG flag
+				Assert::AreEqual(c_finalRegs.m_negativeCondition, false, L"NEG has wrong value.");
+
+				// Assert Zero flag
+				Assert::AreEqual(c_finalRegs.m_zeroCondition, true, L"ZERO has wrong value.");
+
+				// Check memory
+				// Memory cannot change when executing
+				for (int idx = 0; idx < c_outputMemory.size(); idx++)
+				{
+					Assert::AreEqual(c_outputMemory[idx], inputMemory[idx], L"Input and output memory are different.");
+				}
+			}
+
+			TEST_METHOD(JMP_Instruction_1)
+			{
+				// Neander computer instance
+				CComputer neanderComputer;
+
+				// Input memory
+				std::array<uint8_t, CComputer::ms_cMemorySize> inputMemory;
+
+				// Zero initializes memory
+				std::fill(inputMemory.begin(), inputMemory.end(), 0);
+
+				// JMP to mem address 158 where the program halts instead of halting at 3
+				inputMemory[0] = static_cast<uint8_t>(CComputer::EOperations::JMP);
+				inputMemory[1] = 158;
+				// HLT Instruction which is not executed
+				inputMemory[2] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+				// HLT Instruction which is executed
+				inputMemory[158] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+
+				// PC Desired value
+				const uint8_t c_pcFinalValue(159);
+
+				neanderComputer.setMemory(inputMemory);
+
+				neanderComputer.runProgram();
+
+				// Final registers values
+				const SRegisters c_finalRegs(neanderComputer.getRegisters());
+
+				// Final memory values
+				const std::array<uint8_t, CComputer::ms_cMemorySize> c_outputMemory(neanderComputer.getMemory());
+
+				// Assert ACC value
+				Assert::AreEqual(static_cast<int>(c_finalRegs.m_accumulator), 0, L"ACC has wrong value.");
+
+				// Assert PC value
+				Assert::AreEqual(c_finalRegs.m_programCounter, c_pcFinalValue, L"PC has wrong value.");
+
+				// Assert NEG flag
+				Assert::AreEqual(c_finalRegs.m_negativeCondition, false, L"NEG has wrong value.");
+
+				// Assert Zero flag
+				Assert::AreEqual(c_finalRegs.m_zeroCondition, true, L"ZERO has wrong value.");
+
+				// Check memory
+				// Memory cannot change when executing
+				for (int idx = 0; idx < c_outputMemory.size(); idx++)
+				{
+					Assert::AreEqual(c_outputMemory[idx], inputMemory[idx], L"Input and output memory are different.");
+				}
+			}
+
+			TEST_METHOD(JMP_Instruction_2)
+			{
+				// Neander computer instance
+				CComputer neanderComputer;
+
+				// Input memory
+				std::array<uint8_t, CComputer::ms_cMemorySize> inputMemory;
+
+				// Zero initializes memory
+				std::fill(inputMemory.begin(), inputMemory.end(), 0);
+
+				// JMP to mem address 158 where the program halts instead of halting at 3
+				inputMemory[0] = 143;
+				inputMemory[1] = 3;
+				// HLT Instruction which is not executed
+				inputMemory[2] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+				// HLT Instruction which is executed
+				inputMemory[3] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+
+				// PC Desired value
+				const uint8_t c_pcFinalValue(4);
+
+				neanderComputer.setMemory(inputMemory);
+
+				neanderComputer.runProgram();
+
+				// Final registers values
+				const SRegisters c_finalRegs(neanderComputer.getRegisters());
+
+				// Final memory values
+				const std::array<uint8_t, CComputer::ms_cMemorySize> c_outputMemory(neanderComputer.getMemory());
+
+				// Assert ACC value
+				Assert::AreEqual(static_cast<int>(c_finalRegs.m_accumulator), 0, L"ACC has wrong value.");
+
+				// Assert PC value
+				Assert::AreEqual(c_finalRegs.m_programCounter, c_pcFinalValue, L"PC has wrong value.");
+
+				// Assert NEG flag
+				Assert::AreEqual(c_finalRegs.m_negativeCondition, false, L"NEG has wrong value.");
+
+				// Assert Zero flag
+				Assert::AreEqual(c_finalRegs.m_zeroCondition, true, L"ZERO has wrong value.");
+
+				// Check memory
+				// Memory cannot change when executing
+				for (int idx = 0; idx < c_outputMemory.size(); idx++)
+				{
+					Assert::AreEqual(c_outputMemory[idx], inputMemory[idx], L"Input and output memory are different.");
+				}
+			}
+
+			TEST_METHOD(JN_Instruction_1)
+			{
+				// Neander computer instance
+				CComputer neanderComputer;
+
+				// Input memory
+				std::array<uint8_t, CComputer::ms_cMemorySize> inputMemory;
+
+				// Zero initializes memory
+				std::fill(inputMemory.begin(), inputMemory.end(), 0);
+
+				// JN to mem address 158 where the program halts instead of halting at 3
+				inputMemory[0] = static_cast<uint8_t>(CComputer::EOperations::JN);
+				inputMemory[1] = 158;
+				// HLT Instruction which is not executed
+				inputMemory[2] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+				// HLT Instruction which is executed
+				inputMemory[158] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+
+				// PC Desired value
+				const uint8_t c_pcFinalValue(159);
+
+				neanderComputer.setRegisters(SRegisters(128, 0, false, false));
+
+				neanderComputer.setMemory(inputMemory);
+
+				neanderComputer.runProgram();
+
+				// Final registers values
+				const SRegisters c_finalRegs(neanderComputer.getRegisters());
+
+				// Final memory values
+				const std::array<uint8_t, CComputer::ms_cMemorySize> c_outputMemory(neanderComputer.getMemory());
+
+				// Assert ACC value
+				Assert::AreEqual(static_cast<int>(c_finalRegs.m_accumulator), 128, L"ACC has wrong value.");
+
+				// Assert PC value
+				Assert::AreEqual(c_finalRegs.m_programCounter, c_pcFinalValue, L"PC has wrong value.");
+
+				// Assert NEG flag
+				Assert::AreEqual(c_finalRegs.m_negativeCondition, true, L"NEG has wrong value.");
+
+				// Assert Zero flag
+				Assert::AreEqual(c_finalRegs.m_zeroCondition, false, L"ZERO has wrong value.");
+
+				// Check memory
+				// Memory cannot change when executing
+				for (int idx = 0; idx < c_outputMemory.size(); idx++)
+				{
+					Assert::AreEqual(c_outputMemory[idx], inputMemory[idx], L"Input and output memory are different.");
+				}
+			}
+
+			TEST_METHOD(JN_Instruction_2)
+			{
+				// Neander computer instance
+				CComputer neanderComputer;
+
+				// Input memory
+				std::array<uint8_t, CComputer::ms_cMemorySize> inputMemory;
+
+				// Zero initializes memory
+				std::fill(inputMemory.begin(), inputMemory.end(), 0);
+
+				// Halts at 3
+				inputMemory[0] = 159;
+				inputMemory[1] = 158;
+				// HLT Instruction which is not executed
+				inputMemory[2] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+				// HLT Instruction which is executed
+				inputMemory[158] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+
+				// PC Desired value
+				const uint8_t c_pcFinalValue(3);
+
+				neanderComputer.setRegisters(SRegisters(5, 0, false, false));
+
+				neanderComputer.setMemory(inputMemory);
+
+				neanderComputer.runProgram();
+
+				// Final registers values
+				const SRegisters c_finalRegs(neanderComputer.getRegisters());
+
+				// Final memory values
+				const std::array<uint8_t, CComputer::ms_cMemorySize> c_outputMemory(neanderComputer.getMemory());
+
+				// Assert ACC value
+				Assert::AreEqual(static_cast<int>(c_finalRegs.m_accumulator), 5, L"ACC has wrong value.");
+
+				// Assert PC value
+				Assert::AreEqual(c_finalRegs.m_programCounter, c_pcFinalValue, L"PC has wrong value.");
+
+				// Assert NEG flag
+				Assert::AreEqual(c_finalRegs.m_negativeCondition, false, L"NEG has wrong value.");
+
+				// Assert Zero flag
+				Assert::AreEqual(c_finalRegs.m_zeroCondition, false, L"ZERO has wrong value.");
+
+				// Check memory
+				// Memory cannot change when executing
+				for (int idx = 0; idx < c_outputMemory.size(); idx++)
+				{
+					Assert::AreEqual(c_outputMemory[idx], inputMemory[idx], L"Input and output memory are different.");
+				}
+			}
+
+			TEST_METHOD(JZ_Instruction_1)
+			{
+				// Neander computer instance
+				CComputer neanderComputer;
+
+				// Input memory
+				std::array<uint8_t, CComputer::ms_cMemorySize> inputMemory;
+
+				// Zero initializes memory
+				std::fill(inputMemory.begin(), inputMemory.end(), 0);
+
+				// JZ to mem address 158 where the program halts instead of halting at 3
+				inputMemory[0] = static_cast<uint8_t>(CComputer::EOperations::JZ);
+				inputMemory[1] = 158;
+				// HLT Instruction which is not executed
+				inputMemory[2] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+				// HLT Instruction which is executed
+				inputMemory[158] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+
+				// PC Desired value
+				const uint8_t c_pcFinalValue(159);
+
+				neanderComputer.setMemory(inputMemory);
+
+				neanderComputer.runProgram();
+
+				// Final registers values
+				const SRegisters c_finalRegs(neanderComputer.getRegisters());
+
+				// Final memory values
+				const std::array<uint8_t, CComputer::ms_cMemorySize> c_outputMemory(neanderComputer.getMemory());
+
+				// Assert ACC value
+				Assert::AreEqual(static_cast<int>(c_finalRegs.m_accumulator), 0, L"ACC has wrong value.");
+
+				// Assert PC value
+				Assert::AreEqual(c_finalRegs.m_programCounter, c_pcFinalValue, L"PC has wrong value.");
+
+				// Assert NEG flag
+				Assert::AreEqual(c_finalRegs.m_negativeCondition, false, L"NEG has wrong value.");
+
+				// Assert Zero flag
+				Assert::AreEqual(c_finalRegs.m_zeroCondition, true, L"ZERO has wrong value.");
+
+				// Check memory
+				// Memory cannot change when executing
+				for (int idx = 0; idx < c_outputMemory.size(); idx++)
+				{
+					Assert::AreEqual(c_outputMemory[idx], inputMemory[idx], L"Input and output memory are different.");
+				}
+			}
+
+			TEST_METHOD(JZ_Instruction_2)
+			{
+				// Neander computer instance
+				CComputer neanderComputer;
+
+				// Input memory
+				std::array<uint8_t, CComputer::ms_cMemorySize> inputMemory;
+
+				// Zero initializes memory
+				std::fill(inputMemory.begin(), inputMemory.end(), 0);
+
+				// Halts at 3
+				inputMemory[0] = 239;
+				inputMemory[1] = 158;
+				// HLT Instruction which is not executed
+				inputMemory[2] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+				// HLT Instruction which is executed
+				inputMemory[158] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+
+				// PC Desired value
+				const uint8_t c_pcFinalValue(3);
+
+				neanderComputer.setRegisters(SRegisters(5, 0, false, false));
+
+				neanderComputer.setMemory(inputMemory);
+
+				neanderComputer.runProgram();
+
+				// Final registers values
+				const SRegisters c_finalRegs(neanderComputer.getRegisters());
+
+				// Final memory values
+				const std::array<uint8_t, CComputer::ms_cMemorySize> c_outputMemory(neanderComputer.getMemory());
+
+				// Assert ACC value
+				Assert::AreEqual(static_cast<int>(c_finalRegs.m_accumulator), 5, L"ACC has wrong value.");
+
+				// Assert PC value
+				Assert::AreEqual(c_finalRegs.m_programCounter, c_pcFinalValue, L"PC has wrong value.");
+
+				// Assert NEG flag
+				Assert::AreEqual(c_finalRegs.m_negativeCondition, false, L"NEG has wrong value.");
+
+				// Assert Zero flag
+				Assert::AreEqual(c_finalRegs.m_zeroCondition, false, L"ZERO has wrong value.");
+
+				// Check memory
+				// Memory cannot change when executing
+				for (int idx = 0; idx < c_outputMemory.size(); idx++)
+				{
+					Assert::AreEqual(c_outputMemory[idx], inputMemory[idx], L"Input and output memory are different.");
+				}
+			}
+
+			TEST_METHOD(HLT_Instruction_1)
+			{
+				// Neander computer instance
+				CComputer neanderComputer;
+
+				// Input memory
+				std::array<uint8_t, CComputer::ms_cMemorySize> inputMemory;
+
+				// Zero initializes memory
+				std::fill(inputMemory.begin(), inputMemory.end(), 0);
+
+				// Halt
+				inputMemory[0] = static_cast<uint8_t>(CComputer::EOperations::HLT);
+
+				// PC Desired value
+				const uint8_t c_pcFinalValue(1);
+
+				neanderComputer.setMemory(inputMemory);
+
+				neanderComputer.runProgram();
+
+				// Final registers values
+				const SRegisters c_finalRegs(neanderComputer.getRegisters());
+
+				// Final memory values
+				const std::array<uint8_t, CComputer::ms_cMemorySize> c_outputMemory(neanderComputer.getMemory());
+
+				// Assert ACC value
+				Assert::AreEqual(static_cast<int>(c_finalRegs.m_accumulator), 0, L"ACC has wrong value.");
+
+				// Assert PC value
+				Assert::AreEqual(c_finalRegs.m_programCounter, c_pcFinalValue, L"PC has wrong value.");
+
+				// Assert NEG flag
+				Assert::AreEqual(c_finalRegs.m_negativeCondition, false, L"NEG has wrong value.");
+
+				// Assert Zero flag
+				Assert::AreEqual(c_finalRegs.m_zeroCondition, true, L"ZERO has wrong value.");
+
+				// Check memory
+				// Memory cannot change when executing
+				for (int idx = 0; idx < c_outputMemory.size(); idx++)
+				{
+					Assert::AreEqual(c_outputMemory[idx], inputMemory[idx], L"Input and output memory are different.");
+				}
+			}
+
+			TEST_METHOD(HLT_Instruction_2)
+			{
+				// Neander computer instance
+				CComputer neanderComputer;
+
+				// Input memory
+				std::array<uint8_t, CComputer::ms_cMemorySize> inputMemory;
+
+				// Zero initializes memory
+				std::fill(inputMemory.begin(), inputMemory.end(), 0);
+
+				// Halt
+				inputMemory[0] = 255;
+
+				// PC Desired value
+				const uint8_t c_pcFinalValue(1);
+
+				neanderComputer.setMemory(inputMemory);
+
+				neanderComputer.runProgram();
+
+				// Final registers values
+				const SRegisters c_finalRegs(neanderComputer.getRegisters());
+
+				// Final memory values
+				const std::array<uint8_t, CComputer::ms_cMemorySize> c_outputMemory(neanderComputer.getMemory());
+
+				// Assert ACC value
+				Assert::AreEqual(static_cast<int>(c_finalRegs.m_accumulator), 0, L"ACC has wrong value.");
+
+				// Assert PC value
+				Assert::AreEqual(c_finalRegs.m_programCounter, c_pcFinalValue, L"PC has wrong value.");
 
 				// Assert NEG flag
 				Assert::AreEqual(c_finalRegs.m_negativeCondition, false, L"NEG has wrong value.");
